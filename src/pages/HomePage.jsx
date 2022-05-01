30dca7a646b7bc273dee4222bdff7beca23844f4
Joeri <joerischenk@outlook.com>
2022-05-01 15:42:28 +0200

    started working on the homescreen

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Cards, Logo } from "../assets";
 import CardHome from "../assets/homecard.png";
 
 const HomePage = () => {
   return (
     <div id="home" className="justify-between flex flex-col">
-      <div className="flex justify-center md:w-screen md:h-full md:justify-end md:absolute">
+      <div className="px-5 py-10 flex justify-center md:w-screen md:h-full md:justify-end md:absolute">
         <img
-          src={CardHome}
+          src={Cards}
           alt=""
-          className="w-3/4 sm:w-1/2 md:object-contain md:w-1/2 md:flex md:justify-start"
+          className="sm:w-1/2 md:object-contain md:w-1/2 md:flex md:justify-start"
         />
       </div>
-
-      <div className="flex justify-center my-10 md:relative md:w-1/2 md:h-screen md:mx-0 md:bg-java md:py-80 md:items-center">
-        <div className="flex flex-col text-left text-white gap-3">
-          <h3 className="text-4xl font-medium">
-            Banking
-            <span className="text-java-500">
-              {" "}
-              from <br /> another{" "}
-            </span>
-            universe
-          </h3>
-          <p className="text-md text-white">
-            It's time to get a new experience <br /> of interacting with the
-            bank
-          </p>
+      <div className="md:relative md:w-1/2 md:h-screen md:mx-0 md:bg-java md:py-80 md:items-center">
+        <div className="flex justify-center my-10">
+          <div className="flex flex-col text-left text-white gap-3">
+            <div className="flex items-center">
+              <img src={Logo} alt="" className="w-20" />
+            </div>
+            <h3 className="text-4xl font-medium">
+              Banking
+              <span className="text-java-500 md:text-black">
+                {" "}
+                from <br /> another{" "}
+              </span>
+              universe
+            </h3>
+            <p className="text-md text-white">
+              It's time to get a new experience <br /> of interacting with the
+              bank
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center mb-10 md:hidden">
-        <Link to="/welcome" className="py-5 px-32 bg-white rounded-2xl">
-          Sign In
-        </Link>
+        <div className="flex px-10 justify-center mb-10">
+          <Link
+            to="/welcome"
+            className="py-4 w-full text-center bg-white rounded-2xl"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
